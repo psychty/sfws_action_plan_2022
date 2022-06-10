beb59ad6c07133e7fcb9937c851aa070cd03f8b5
Rich Tyler <rich.tyler.2018@hotmail.com>
2022-06-10 10:14:48 +0100

    small change to content

--- sfws_app/src/components/key_points.jsx
+++ sfws_app/src/components/key_points.jsx
@@ -7,7 +7,7 @@ function Key_points() {
 
             <p>This page shows the 2019-22 Smokefree West Sussex Tobacco Control Strategy actions. These are a collection of actions agreed by members of the Smokefree West Sussex Partnership to take forward the tobacco control agenda in the county. Actions are coloured by the 2008 Department of Health (now called the Department of Health and Social Care) 10 high impact changes to achieve tobacco control.</p>
             <p>You can view the <a className = 'i_will_show_link' href = 'https://webarchive.nationalarchives.gov.uk/ukgwa/20130123193737/http://www.dh.gov.uk/en/Publicationsandstatistics/Publications/PublicationsPolicyAndGuidance/DH_084847'>Excellence in tobacco control: 10 High Impact Changes to achieve tobacco control report</a> on the government archive website.</p>
-            <p>Click on a high impact change icon below to find out more.</p>
+            <p>Click on a high impact change icon below to find out more and to see the West Sussex actions associated with the change.</p>
         </div>
     )
 }
